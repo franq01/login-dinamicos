672cb9e53a24f4cd32e1cfeb8a83c62bfb4a7b71
franq01 <carreone705@gmail.com>
2023-05-19 12:10:22 -0600

    redireccionamiento de login

---
+++ assets/login2/login1.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //variables
+    $surface = $('.surface');
+    $car = $('.car');
+
+    //keypress event 
+    $(document).on('keypress', function(e){
+        console.log(e.wich);
+    })
+});
